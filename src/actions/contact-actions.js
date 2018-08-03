7c08573a14797f981d5fed7526908a34ca8be03f
Kareem James <kareemdjames@gmail.com>
2018-08-02 21:30:04 -0400

    added create a new user form with server side validation tested successful

--- src/actions/contact-actions.js
+++ src/actions/contact-actions.js
@@ -10,3 +10,22 @@ export function fetchContacts() {
     })
   }
 }
+
+//Provides a new contact object to the Redux form/
+export function newContact() {
+  return dispatch => {
+    dispatch({
+      type: 'NEW_CONTACT'
+    })
+  }
+}
+
+//Post contact data to the api server
+export function saveContact(contact) {
+  return dispatch => {
+    return dispatch({
+      type: 'SAVE_CONTACT',
+      payload: client.post(url, contact)
+    })
+  }
+}
